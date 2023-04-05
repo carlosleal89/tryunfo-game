87dbfe4d50f7915eb4cbc4cab255630835620e3f
Carlos Leal <carlos.frleal@gmail.com>
2023-04-05 19:31:40 -0300

    requisito 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,143 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import Input from './Input';
+import './Form.css';
 
 class Form extends React.Component {
   render() {
-    return <Input />;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+    console.log(cardName);
+    return (
+      <section className="input-section">
+        <label htmlFor="name-input">
+          Nome
+          <input
+            type="text"
+            data-testid="name-input"
+            name="name-input"
+            id="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="description-input">
+          Descrição
+          <textarea
+            name="description-input"
+            id="description-input"
+            cols="20"
+            rows="10"
+            data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="attr1-input">
+          Attr01
+          <input
+            type="number"
+            name="attr1-input"
+            id="attr1-input"
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="attr2-input">
+          Attr02
+          <input
+            type="number"
+            name="attr2-input"
+            id="attr2-input"
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="attr3-input">
+          Attr03
+          <input
+            type="number"
+            name="attr3-input"
+            id="attr3-input"
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="image-input">
+          Imagem
+          <input
+            type="text"
+            name="image-input"
+            id="image-input"
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="rare-input">
+          Raridade
+          <select
+            name="rare-input"
+            id="rare-input"
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="trunfo-input">
+          Super Trunfo
+          <input
+            type="checkbox"
+            name="trunfo-input"
+            id="trunfo-input"
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
+        </label>
+        <button
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
+      </section>
+    );
   }
 }
+
+Form.propTypes = {
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardName: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  hasTrunfo: PropTypes.bool,
+  isSaveButtonDisabled: PropTypes.bool,
+  onInputChange: PropTypes.callback,
+  onSaveButtonClick: PropTypes.callback,
+}.isRequired;
 
 export default Form;
