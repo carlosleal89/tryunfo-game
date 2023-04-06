c0628ad89c96f6701712857ba84fd57af9647ce1
Carlos Leal <carlos.frleal@gmail.com>
2023-04-06 20:51:45 -0300

    requisito 8

--- src/App.js
+++ src/App.js
@@ -63,7 +63,6 @@ class App extends React.Component {
 
   saveButton = () => {
     const card = this.state;
-
     this.setState((prevState) => ({
       cardName: '',
       cardDescription: '',
@@ -90,6 +89,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      cardsArray,
       isSaveButtonDisabled,
     } = this.state;
     return (
@@ -119,6 +119,21 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {
+          cardsArray.map((card, index) => (
+            <Card
+              key={ index }
+              cardName={ card.cardName }
+              cardDescription={ card.cardDescription }
+              cardImage={ card.cardImage }
+              cardAttr1={ card.cardAttr1 }
+              cardAttr2={ card.cardAttr2 }
+              cardAttr3={ card.cardAttr3 }
+              cardRare={ card.cardRare }
+              cardTrunfo={ card.cardTrunfo }
+            />
+          ))
+        }
       </div>
     );
   }
