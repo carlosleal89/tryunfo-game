a37225f4f881696236dd816be90a20e49266d66d
Carlos Leal <carlos.frleal@gmail.com>
2023-04-06 10:24:20 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -12,6 +12,47 @@ class App extends React.Component {
     cardAttr3: '',
     cardRare: 'normal',
     cardTrunfo: false,
+    isSaveButtonDisabled: true,
+  };
+
+  // criar um evento para quando passar o mouse em cima do botão salvar desabilitado, ele mostrar um mensagem informando quais campos faltam preencher
+  errorHandler = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const MAX_ATTR_VAL = 90;
+    const MAX_TOTAL_ATTR_VAL = 210;
+    const valCardName = cardName.length > 0; // atribui um valor boolean a cada const.
+    const valCardDescription = cardDescription.length > 0;
+    const valCardImage = cardImage.length > 0;
+    const valCardRare = cardRare !== 'normal';
+    const valAttr1 = (Number(cardAttr1) >= 0 && Number(cardAttr1) <= MAX_ATTR_VAL);
+    const valAttr2 = (Number(cardAttr2) >= 0 && Number(cardAttr2) <= MAX_ATTR_VAL);
+    const valAttr3 = (Number(cardAttr3) >= 0 && Number(cardAttr3) <= MAX_ATTR_VAL);
+    const valMaxAttrVal = (
+      (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= MAX_TOTAL_ATTR_VAL)
+    );
+    console.log(Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3));
+
+    const valInputs = (valCardName && valCardDescription && valCardImage && valCardRare);
+    const valAttr = (
+      valAttr1 && valAttr2 && valAttr3 && valCardDescription && valMaxAttrVal
+    );
+
+    this.setState({
+      isSaveButtonDisabled: !(
+        valInputs && valAttr // se todas const retornarem true, o botão vai ser habilitado( a logica ta invertida pelo ! antes da expressão )
+      ),
+    });
+
+    // console.log(valCardName, valCardDescription, valCardImage, valCardRare);
   };
 
   onInputChange = ({ target }) => {
@@ -20,7 +61,7 @@ class App extends React.Component {
     console.log(value);
     this.setState({
       [name]: value,
-    });
+    }, this.errorHandler);
   };
 
   render() {
@@ -33,6 +74,7 @@ class App extends React.Component {
       cardAttr3,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -47,6 +89,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
